1ff7beb9e4e60476d90c4e09d34e9c773f472a90
Joan Tudela <jotudela@c2r6p9.42perpignan.fr>
2025-01-08 17:29:10 +0100

    OK

--- push_swap/mandatory/main.c
+++ push_swap/mandatory/main.c
@@ -6,18 +6,25 @@
 /*   By: jotudela <jotudela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:13:07 by jotudela          #+#    #+#             */
-/*   Updated: 2025/01/08 15:02:36 by jotudela         ###   ########.fr       */
+/*   Updated: 2025/01/08 17:28:54 by jotudela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+void    printlst(t_list *li)
+{
+    while (li)
+    {
+        ft_printf("%i\n", li->value);
+        li = li->next;
+    }
+}
+
 int main(int ac, char **av)
 {
     t_list *a = ft_init(ac, av);
-
-    ft_printlst(a);
+    printlst(a);
     ft_lstclear(&a);
-
     return (0);
 }
