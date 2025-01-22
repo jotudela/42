80766fe50c136cb5a7d530b8f68a2b058d4cc201
Joan Tudela <jotudela@c1r1p10.42perpignan.fr>
2025-01-22 13:36:09 +0100

    ALL GOOD

--- so_long/mandatory/check/good_len_line.c
+++ so_long/mandatory/check/good_len_line.c
@@ -6,18 +6,27 @@
 /*   By: jotudela <jotudela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 15:21:03 by jotudela          #+#    #+#             */
-/*   Updated: 2025/01/21 14:27:05 by jotudela         ###   ########.fr       */
+/*   Updated: 2025/01/22 13:20:25 by jotudela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/so_long.h"
 
+static void	last_len(t_map *map)
+{
+	t_map	*tmp;
+
+	tmp = ft_lstlast(map);
+	tmp->len_line += 1;
+}
+
 int	good_len_line(t_map *map, t_control *su)
 {
 	int	tmp;
 
+	last_len(map);
 	tmp = map->len_line;
-	while (map->next)
+	while (map)
 	{
 		if (tmp != map->len_line)
 		{
