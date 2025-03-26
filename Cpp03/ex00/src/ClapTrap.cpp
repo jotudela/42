b7b0741254f410f1e0940879c7add27e8df761e9
Joan Tudela <jotudela@c3r4p4.42perpignan.fr>
2025-03-26 11:40:30 +0100

    First exo OK

---
+++ Cpp03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotudela <jotudela@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 10:11:56 by jotudela          #+#    #+#             */
+/*   Updated: 2025/03/26 11:35:44 by jotudela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ClapTrap.hpp"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+ClapTrap::ClapTrap( string n ): name(n), health(10), energy(10), damage(0)
+{
+    cout << "ClapTrap " << name << " has created !" << endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+    cout << "ClapTrap " << name << " has destroyed !" << endl;
+}
+
+ClapTrap& ClapTrap::operator=( const ClapTrap& other )
+{
+    if (this != &other){
+        cout << "ClapTrap " << this->name << " is now " << other.name << " !" << endl;
+        this->name = other.name;
+        this->health = other.health;
+        this->energy = other.energy;
+        this->damage = other.damage;
+    }
+    return *this;
+}
+
+ClapTrap::ClapTrap( ClapTrap const& copy )
+{
+    cout << "Copy constructor called" << endl;
+    *this = copy;
+}
+
+void ClapTrap::attack( const string& target )
+{
+    if (this->damage == 0){
+        cout << "ClapTrap " << this->name << " can't do damage !" << endl;
+        return ;
+    }
+    if (this->energy <= 0){
+        cout << "ClapTrap " << this->name << " has no energy to do anythings !" << endl;
+        return ;
+    }
+    if (this->health <= 0){
+        cout << "ClapTrap " << this->name << " has no health !" << endl;
+        return ;
+    }
+    this->energy -= 1;
+    cout << "ClapTrap "
+        << this->name
+        << " attacks "
+        << target
+        << ", causing "
+        << this->damage
+        << " points of damage !" << endl;
+}
+
+void ClapTrap::takeDamage( unsigned int amount )
+{
+    if (this->health <= 0){
+        cout << "ClapTrap " << this->name << " has no Health !" << endl;
+        return ;
+    }
+    cout << "ClapTrap "
+        << this->name
+        << " take "
+        << amount
+        << " damage, now he have "
+        << this->health
+        << " Health !" <<endl;
+}
+
+void ClapTrap::beRepaired( unsigned int amount )
+{
+    if (this->energy <= 0){
+        cout << "ClapTrap " << this->name << " has no energy to do anythings !" << endl;
+        return ;
+    }
+    if (this->health <= 0){
+        cout << "ClapTrap " << this->name << " has no health !" << endl;
+        return ;
+    }
+    if (this->health == 10){
+        cout << "ClapTrap " << this->name << " has max Health !" << endl;
+        return ;
+    }
+    this->energy -= 1;
+    cout << "ClapTrap "
+        << this->name
+        << "repaire him "
+        << amount
+        << " health, now he have "
+        << this->health
+        << " Health !" << endl;
+}
