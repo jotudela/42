a9a58f0cf3fbc7d317b30d76b8ff6dee6c920065
Joan Tudela <jotudela@c3r4p4.42perpignan.fr>
2025-05-21 13:40:34 +0200

    ex01 ok

---
+++ Cpp06/ex01/inc/Serializer.hpp
@@ -0,0 +1,21 @@
+#ifndef _SERIALIZER_HPP_
+# define _SERIALIZER_HPP_
+
+# include <iostream>
+# include <stdint.h>
+
+typedef struct s_data {} Data;
+
+class Serializer
+{
+    private:
+        Serializer();
+        Serializer( const Serializer& );
+        Serializer& operator=( const Serializer& );
+        ~Serializer();
+    public:
+        static uintptr_t serialize( Data* ptr );
+        static Data* deserialize( uintptr_t raw );
+};
+
+#endif
